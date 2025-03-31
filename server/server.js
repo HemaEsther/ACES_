65ce42a0611f14669427c19ee5a2ccf0393eb4a0
Ahmad Kaif <heyahmad611@gmail.com>
2025-03-31 14:17:38 +0530

    .

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser()); // Required for handling cookies
 app.use(
   cors({
-    origin: "https://resume-builder-six-fawn.vercel.app/", // ✅ Allow frontend origin
+    origin: "https://resume-builder-six-fawn.vercel.app", // ✅ Allow frontend origin
     credentials: true, // ✅ Allow cookies to be sent
   })
 );
