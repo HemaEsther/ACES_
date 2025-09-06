e7c9b29f58ca82e28f1f7de5bf76702dd5db9113
China Brahma <brahmachina143@gmail.com>
2025-09-06 13:28:48 +0530

    puppeteer core eliminated

--- server/controllers/handleResumeController.js
+++ server/controllers/handleResumeController.js
@@ -1,14 +1,8 @@
 import fs from "fs";
 import axios from "axios";
-<<<<<<< HEAD
-import path from "path";
-import pdfjsLib from "pdfjs-dist/legacy/build/pdf.js";
-=======
 // import pdfParse from "pdf-parse";   --------- issue with this
 import pdfParse from "pdf-parse-fixed";
 
-
->>>>>>> b8cc275f8aad853702bc2eaffc3c2065f874dbff
 
 export const handleResume = async (req, res) => {
   console.log(req.body);
@@ -26,48 +20,22 @@ export const handleResume = async (req, res) => {
   let resumeText = "";
 
   try {
-<<<<<<< HEAD
-    const filePath = path.resolve(req.file.path);
-    const pdfBuffer = fs.readFileSync(filePath);
-
-    // ✅ Convert Buffer -> Uint8Array
-    const pdfData = new Uint8Array(pdfBuffer);
-
-    const pdf = await pdfjsLib.getDocument({ data: pdfData }).promise;
-
-    for (let i = 1; i <= pdf.numPages; i++) {
-      const page = await pdf.getPage(i);
-      const content = await page.getTextContent();
-      const pageText = content.items.map(item => item.str).join(" ");
-      resumeText += pageText + "\n";
-    }
-
-    fs.unlinkSync(filePath); // Clean up uploaded file
-=======
     const pdfData = await pdfParse(fs.readFileSync(req.file.path));
     resumeText = pdfData.text;
     fs.unlinkSync(req.file.path);
->>>>>>> b8cc275f8aad853702bc2eaffc3c2065f874dbff
   } catch (error) {
     console.error("PDF Parsing Error ❌:", error);
     return res.status(500).json({ error: "Error processing resume file 😭😭😭" });
   }
 
-<<<<<<< HEAD
-  const local_url_python_backend = "http://localhost:5002/predict";
-
-  try {
-    const response = await axios.post(local_url_python_backend, {
-=======
   // const docker_url_python_backend='http://ml-model:5002/predict';
-  // const local_url_python_backend='http://localhost:5002/predict';
-  const production_url_python_backend='https://aces-kth9.onrender.com/predict';
+  const local_url_python_backend='http://localhost:5002/predict';
+  // const production_url_python_backend='https://aces-kth9.onrender.com/predict';
 
   try {
     // localhost kam nahi kar raha 
     console.log("Helooooooooo")
-    const response = await axios.post(`${production_url_python_backend}`, {
->>>>>>> b8cc275f8aad853702bc2eaffc3c2065f874dbff
+    const response = await axios.post(`${local_url_python_backend}`, {
       resume_text: resumeText,
       job_desc,
       role,
